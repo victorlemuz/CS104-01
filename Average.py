a29eb503b581806cc2098b6d2d3ca67832bc6f9d
victorlemuz <71726073+victorlemuz@users.noreply.github.com>
2020-09-30 23:01:22 -0400

    This is the third commit with the loop to average.py

--- Average.py
+++ Average.py
@@ -1,16 +1,18 @@
 numberOfTests = 0
 score = 0
-total = 0.0
+total = 0
 average = 0.0
 scoreCount = 0
 
-numberofTests = int(input("Please enter the number of test you want to average: "))
+numberOfTests = int(input("Please enter the number of test you want to average: "))
 
 #Make these next three lines repeat until scoreCount = numberOfTests
-score = int(input("Please enter a score: "))
-scoreCount = scoreCount + 1
-total = total + score
-
-
-average = total/scpreCount
+while scoreCount < numberOfTests:
+    score1 = int(input("Please enter a score: "))
+    scoreCount = scoreCount + 1
+    total = (score1 + total)
+    if scoreCount == numberOfTests:
+        break
+    
+average = total / numberOfTest
 print("The average is ", average)
